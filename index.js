b835bdf98e1ae7db3c63e30ceb8b14c47f09f697
seyoungb <se.young492@gmail.com>
2021-03-23 20:00:28 +0900

    20210323_2-8

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('Hi!');
+import "core-js";
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser"; 
+
+const app = express();
+
+const PORT = 4000;
+
+const handleProfile = (req,res) => res.send('you are my profile');
+const handleHome = (req,res) =>  res.send('hello from ass');
+const handleListening = () =>  
+    console.log(`Listening on: http://localhost:${PORT}`);
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+
+app.get('/',handleHome);
+app.get('/profile',handleProfile);
+app.listen(4000,handleListening);
